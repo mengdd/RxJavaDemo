7434a909b18fc172f47a9ebabcb0cb28ff371d00
Dandan MENG <mengdandanno1@163.com>
2016-06-01 10:51:57 +0800

    send multiple requests and handle separately using flatMap()

--- app/src/main/java/com/ddmeng/rxjavademo/BasicRequestHandleActivity.java
+++ app/src/main/java/com/ddmeng/rxjavademo/BasicRequestHandleActivity.java
@@ -6,14 +6,13 @@ import android.util.Log;
 
 import com.ddmeng.rxjavademo.network.MockFetcher;
 
-import java.io.IOException;
-
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import rx.Observable;
 import rx.Observer;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
 
 import static rx.schedulers.Schedulers.io;
 
@@ -27,8 +26,8 @@ public class BasicRequestHandleActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.button_send)
-    void onSendClick() {
+    @OnClick(R.id.button_send_one)
+    void onSendOneClick() {
         Observable.create(new Observable.OnSubscribe<String>() {
             @Override
             public void call(Subscriber<? super String> subscriber) {
@@ -51,11 +50,6 @@ public class BasicRequestHandleActivity extends AppCompatActivity {
             @Override
             public void onError(Throwable e) {
                 Log.i(TAG, "onError()");
-                if (e instanceof IOException) {
-                    Log.i(TAG, "IOException");
-                } else {
-                    Log.i(TAG, "Not IOException");
-                }
             }
 
             @Override
@@ -64,5 +58,46 @@ public class BasicRequestHandleActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+
+    @OnClick(R.id.button_send_multiple)
+    void onSendMultipleClick() {
+        String[] urls = new String[]{"url1", "url2", "url3"};
+        Observable.from(urls)
+                .flatMap(new Func1<String, Observable<String>>() {
+                    @Override
+                    public Observable<String> call(final String url) {
+                        return Observable.create(new Observable.OnSubscribe<String>() {
+                            @Override
+                            public void call(Subscriber<? super String> subscriber) {
+                                try {
+                                    Log.i(TAG, "sending request to " + url);
+                                    String response = MockFetcher.getString(url);
+                                    subscriber.onNext(response);
+                                    subscriber.onCompleted();
+                                } catch (Exception e) {
+                                    Log.i(TAG, "Error occurs when sending request to " + url + " exception: ", e);
+                                    subscriber.onError(e);
+                                }
+                            }
+                        }).subscribeOn(io());
+                    }
+                }).subscribeOn(AndroidSchedulers.mainThread()).subscribe(new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+                Log.i(TAG, "onCompleted()");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.i(TAG, "onError()");
+            }
+
+            @Override
+            public void onNext(String response) {
+                Log.i(TAG, "onNext(): " + response);
+            }
+        });
     }
 }
