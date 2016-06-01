22627530b97de301277ea6e6922ba081944375dc
Dandan MENG <mengdandanno1@163.com>
2016-06-01 09:58:17 +0800

    add sample list and ButterKnife

--- app/src/main/java/com/ddmeng/rxjavademo/MainActivity.java
+++ app/src/main/java/com/ddmeng/rxjavademo/MainActivity.java
@@ -1,13 +1,54 @@
 package com.ddmeng.rxjavademo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnItemClick;
 
 public class MainActivity extends AppCompatActivity {
+    private static Sample[] samples;
+    @BindView(R.id.samples_list)
+    ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        samples = new Sample[]{
+        };
+
+        mListView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, samples));
+    }
+
+    @OnItemClick(R.id.samples_list)
+    void onSampleListClick(AdapterView<?> parent, View view, int position, long id) {
+        // Launch the sample associated with this list position.
+        startActivity(new Intent(MainActivity.this, samples[position].activityClass));
+    }
+
+
+    private class Sample {
+        private CharSequence title;
+        private Class<? extends Activity> activityClass;
+
+        public Sample(int titleResId, Class<? extends Activity> activityClass) {
+            this.activityClass = activityClass;
+            this.title = getResources().getString(titleResId);
+        }
+
+        @Override
+        public String toString() {
+            return title.toString();
+        }
     }
 }
